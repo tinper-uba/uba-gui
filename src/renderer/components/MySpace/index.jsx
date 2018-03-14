5730b1f08f46c2f76da0901544fd33d922106669
kvkens <kvkens@qq.com>
2018-03-15 00:36:46 +0800

    add:cmd line

--- src/renderer/components/MySpace/index.jsx
+++ src/renderer/components/MySpace/index.jsx
@@ -4,6 +4,7 @@ import { Button, notification, message, Layout,List, Avatar } from 'antd';
 import { ipcRenderer } from 'electron';
 import LeftMenu from 'components/LeftMenu';
 import Logo from 'components/Logo';
+import ut from 'static/ut.png';
 
 import './index.less';
 const { Header, Content, Footer, Sider } = Layout;
@@ -29,21 +30,41 @@ class MySpace extends Component {
         return (
             <div className="uba-my-space">
                 <Layout>
-                    <Content>
+                    <Content style={{"background":"#fff"}}>
                         <List
-                            className="demo-loadmore-list"
+                            className="uba-project-list"
                             itemLayout="horizontal"
+                            bordered={true}
                             dataSource={workSpace}
                             renderItem={item => (
-                                <List.Item actions={[<a>edit</a>, <a>more</a>]}>
+                                <List.Item actions={[
+                                    <Button type="primary">运行</Button>,
+                                    <Button type="primary">构建</Button>,
+                                    <Button type="danger">删除</Button>
+                                ]}>
                                     <List.Item.Meta
-                                        avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
-                                        title={<a href="https://ant.design">{item.title}</a>}
-                                        description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+                                        avatar={<Avatar src={ut} />}
+                                        title={item.title}
+                                        description={`Path:${item.path}`}
                                     />
                                 </List.Item>
                             )}
                         />
+                        <div className="uba-cmd">
+                        [2018-2-4 0:26:35] 开始检测uba本地配置文件<br />
+                        [2018-2-4 0:26:35] 配置存在，读取显示工作区并切换组件，发送IPC消息 uba::view::project<br />
+                        [2018-2-4 0:33:3] 开始检测uba本地配置文件<br />
+                        [2018-2-4 0:33:3] 配置存在，读取显示工作区并切换组件，发送IPC消息 uba::view::project<br />
+                        [2018-2-4 0:26:35] 开始检测uba本地配置文件<br />
+                        [2018-2-4 0:26:35] 配置存在，读取显示工作区并切换组件，发送IPC消息 uba::view::project<br />
+                        [2018-2-4 0:33:3] 开始检测uba本地配置文件<br />
+                        [2018-2-4 0:33:3] 配置存在，读取显示工作区并切换组件，发送IPC消息 uba::view::project<br />
+                        [2018-2-4 0:26:35] 开始检测uba本地配置文件<br />
+                        [2018-2-4 0:26:35] 配置存在，读取显示工作区并切换组件，发送IPC消息 uba::view::project<br />
+                        [2018-2-4 0:33:3] 开始检测uba本地配置文件<br />
+                        [2018-2-4 0:33:3] 配置存在，读取显示工作区并切换组件，发送IPC消息 uba::view::project<br />
+                        
+                        </div>
                     </Content>
                 </Layout>
             </div>
