60f076ef0e3763c2bc1d1ebe1fa36f1235dd52da
kvkens <kvkens@qq.com>
2018-05-02 15:58:17 +0800

    完善可视化Mock的增删改查.

--- src/renderer/components/MockData/index.jsx
+++ src/renderer/components/MockData/index.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import mirror, { actions, connect } from 'mirrorx';
-import { Row, Col, Button, Icon, Table, Input, Select } from 'antd';
+import { Row, Col, Button, Icon, Table, Input, Select, Popconfirm } from 'antd';
 import { ipcRenderer, remote } from 'electron';
 import util from 'common';
 import uuid from 'uuid';
@@ -22,14 +22,14 @@ const EditableCellSelect = ({ editable, value, onChange }) => (
   <div>
     {editable
       ? <Select size="small" defaultValue={value} style={{ width: 90 }} onChange={value => onChange(value)}>
-          <Option value="GET">GET</Option>
-          <Option value="POST">POST</Option>
-          <Option value="DELETE">DELETE</Option>
-          <Option value="PUT">PUT</Option>
-          <Option value="PATCH">PATCH</Option>
-          <Option value="HEAD">HEAD</Option>
-          <Option value="OPTIONS">OPTIONS</Option>
-        </Select>
+        <Option value="GET">GET</Option>
+        <Option value="POST">POST</Option>
+        <Option value="DELETE">DELETE</Option>
+        <Option value="PUT">PUT</Option>
+        <Option value="PATCH">PATCH</Option>
+        <Option value="HEAD">HEAD</Option>
+        <Option value="OPTIONS">OPTIONS</Option>
+      </Select>
       : value
     }
   </div>
@@ -60,14 +60,18 @@ class MockData extends Component {
       title: '操作',
       width: '20%',
       key: 'action',
-      render: (text, record) => {
+      render: (text, record, index) => {
         const { editable } = record;
         return (<span>
           {editable ? <span>
-            <Button onClick={() => this.save(record.key)} size="small">保存</Button>
-            <Button onClick={() => this.cancel(record.key)} size="small">取消</Button>
+            <Button type="primary" style={{ 'marginRight': '4px' }} onClick={() => this.save(record.key)} size="small">保存</Button>
+            <Button onClick={() => this.cancel(record.key, index)} size="small">取消</Button>
           </span>
-            : <Button onClick={() => this.edit(record.key)} size="small">编辑</Button>}
+            : <span><Button style={{ 'marginRight': '4px' }} onClick={() => this.edit(record.key)} size="small">编辑</Button>
+              <Popconfirm title="是否确认删除该条Mock?" onConfirm={() => this.remove(index)} okText="删除" cancelText="取消">
+                <Button type="danger" size="small">删除</Button>
+              </Popconfirm></span>
+          }
         </span>)
       }
     }];
@@ -130,6 +134,7 @@ class MockData extends Component {
       });
     }
   }
+
   save = (key) => {
     const newData = [...this.props.mockTableData];
     const target = newData.filter(item => key === item.key)[0];
@@ -139,8 +144,6 @@ class MockData extends Component {
         mockTableData: newData
       });
       this.cacheData = newData.map(item => ({ ...item }));
-      console.log(this.cacheData);
-      console.log('table数据转换为本地数据开始');
       let ubarcMock = {};
       let subArray = [];
       for (let i = 0; i < this.cacheData.length; i++) {
@@ -160,27 +163,85 @@ class MockData extends Component {
     }
   }
 
-  cancel = (key) => {
+  cancel = (key, index) => {
     const newData = [...this.props.mockTableData];
     const target = newData.filter(item => key === item.key)[0];
-    if (target) {
-      Object.assign(target, this.cacheData.filter(item => key === item.key)[0]);
-      delete target.editable;
+    if (target.isNew) {
+      newData.splice(index, 1);
       actions.main.save({
         mockTableData: newData
       });
+    } else {
+      if (target) {
+        Object.assign(target, this.cacheData.filter(item => key === item.key)[0]);
+        delete target.editable;
+        actions.main.save({
+          mockTableData: newData
+        });
+      }
     }
   }
 
   edit = (key) => {
     const newData = [...this.props.mockTableData];
     const target = newData.filter(item => key === item.key)[0];
-    if (target) {
+    if (target.isNew) {
+      delete target.isNew;
       target.editable = true;
       actions.main.save({
         mockTableData: newData
       });
+    } else {
+      if (target) {
+        target.editable = true;
+        actions.main.save({
+          mockTableData: newData
+        });
+      }
     }
+
+  }
+
+  remove = (index) => {
+    const newData = [...this.props.mockTableData];
+    newData.splice(index, 1);
+    actions.main.save({
+      mockTableData: newData
+    });
+    this.cacheData = newData.map(item => ({ ...item }));
+    let ubarcMock = {};
+    let subArray = [];
+    for (let i = 0; i < this.cacheData.length; i++) {
+      let method = this.cacheData[i].method;
+      let path = this.cacheData[i].path;
+      let url = this.cacheData[i].url;
+      if (typeof ubarcMock[method] === 'undefined') {
+        ubarcMock[method] = [];
+      }
+      ubarcMock[method] = ubarcMock[method].concat({
+        [url]: path
+      });
+    }
+    //写入配置
+    util.setUbarc(this.props.runProject, 'mock', ubarcMock);
+
+  }
+
+  handleAddMock = () => {
+    const newData = [...this.props.mockTableData];
+    const item = {
+      editable: true,
+      key: uuid(),
+      method: 'GET',
+      url: '',
+      path: '',
+      isNew: true
+    };
+    newData.push(item);
+    actions.main.save({
+      mockTableData: newData
+    });
+
   }
   render() {
     let { toolbarHeight, mockTableData } = this.props;
@@ -191,9 +252,11 @@ class MockData extends Component {
             <Table
               loading={!mockTableData.length}
               pagination={false}
+              size="small"
               columns={this.columns}
               dataSource={mockTableData}
-              scroll={{ y: toolbarHeight - 100 }}
+              title={() => <Button onClick={this.handleAddMock} type="" style={{ 'marginLeft': '5px' }}>添加数据</Button>}
+              scroll={{ y: toolbarHeight - 150 }}
               rowKey={record => record.key}
             />
           </Col>
